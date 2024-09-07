267e3d270b68fb92b2713c9360d0c126008d584f
Merch7x <aquamaingi@gmail.com>
2024-09-07 11:40:46 +0300

    Add exception test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,7 +10,7 @@ from parameterized import parameterized
 class TestAccessNestedMap(unittest.TestCase):
     """ Tests various method functionalities"""
     @parameterized.expand([
-      ({"a": 1}, ("a"), 1),
+      ({"a": 1}, ("a",), 1),
       ({"a": {"b": 2}}, ("a",), {"b": 2}),
       ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
@@ -20,7 +20,10 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a"),)
-        ({"a": 1}, ("a", "b"),)
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
     ])
-    def test_access_nested_map_exception()
+    def test_access_nested_map_exception(self, nested, path):
+        """Test wether exceptions are raised"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested, path)

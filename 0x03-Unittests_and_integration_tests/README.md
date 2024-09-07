ed2465da0df4ed07140ee9b9d34761232f0fd774
Merch7x <aquamaingi@gmail.com>
2024-09-07 11:28:28 +0300

    Initial files

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,19 @@
+# Project: 0x03. Unittests and Integration Tests
+
+<h2>Learning Objectives</h2>
+
+<h2>Tasks</h2>
+
+| Task | File |
+| ---- | ---- |
+| 0. Parameterize a unit test | [test_utils.py](./test_utils.py) |
+| 1. Parameterize a unit test | [test_utils.py](./test_utils.py) |
+| 2. Mock HTTP calls | [test_utils.py](./test_utils.py) |
+| 3. Parameterize and patch | [test_utils.py](./test_utils.py) |
+| 4. Parameterize and patch as decorators | [test_client.py](./test_client.py) |
+| 5.  Mocking a property | [test_client.py](./test_client.py) |
+| 6. More patching | [test_client.py](./test_client.py) |
+| 7. Parameterize | [test_client.py](./test_client.py) |
+| 8. Integration test: fixtures | [test_client.py](./test_client.py) |
+| 9. Integration tests | [test_client.py](./test_client.py) |
+
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Implementing unit test through the
+   unittest module
+"""
+from utils import access_nested_map
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Tests various method functionalities"""
+    @parameterized.expand([
+      ({"a": 1}, ("a"), 1),
+      ({"a": {"b": 2}}, ("a",), {"b": 2}),
+      ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_acccess_nested_map(self,
+                                nested_map: map, path: tuple, expected: str):
+        """Test whether path leads to key accurately"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a"),)
+        ({"a": 1}, ("a", "b"),)
+    ])
+    def test_access_nested_map_exception()

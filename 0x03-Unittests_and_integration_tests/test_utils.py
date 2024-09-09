64cbf38bf96731c4d00109bd83137bcdeb0b3d1e
Merch7x <aquamaingi@gmail.com>
2024-09-09 16:45:11 +0300

    Test memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,18 +2,18 @@
 """Implementing unit test through the
    unittest module
 """
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 import unittest
 from unittest.mock import patch, MagicMock
 from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ Tests various method functionalities"""
+    """Tests various method functionalities"""
     @parameterized.expand([
-      ({"a": 1}, ("a",), 1),
-      ({"a": {"b": 2}}, ("a",), {"b": 2}),
-      ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_acccess_nested_map(self, nested_map, path, expected):
         """Test whether path leads to key accurately"""
@@ -48,3 +48,30 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(result, payload)
 
         mock_get.assert_called_once_with(url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Testing the memoize decorator"""
+
+    def test_memoize(self):
+        """creates a sample class then mocks it"""
+        class TestClass:
+            """Test class to test"""
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_obj = TestClass()
+        with patch.object(TestClass, 'a_method', return_value=42)\
+                as mock_a_method:
+            first_call_result = test_obj.a_property
+            self.assertEqual(first_call_result, 42)
+
+            second_call_result = test_obj.a_property
+            self.assertEqual(second_call_result, 42)
+
+            mock_a_method.assert_called_once()

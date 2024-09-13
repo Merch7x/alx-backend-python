f8c183810e0478d91ab308ea739e8994c30261fc
Merch7x <aquamaingi@gmail.com>
2024-09-13 13:38:07 +0300

    Update test name:

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,7 +15,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_acccess_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map, path, expected):
         """Test whether path leads to key accurately"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 

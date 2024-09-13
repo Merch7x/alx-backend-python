c720d089198e0a964dbefab39ef6e7ed3e3ec683
Merch7x <aquamaingi@gmail.com>
2024-09-13 20:34:25 +0300

    test public repos url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@
 """
 from client import GithubOrgClient
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized
 
 
@@ -28,3 +28,18 @@ class TestGithubOrgClient(unittest.TestCase):
 
         mock_get.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
+
+    def test_public_repos_url(self):
+        """Test that the repos_url is equal to that in the
+        property dict
+        """
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=PropertyMock)as mock_org:
+            mock_org.return_value = {
+                "repos_url": "https://api.github.com/orgs/google/repos"}
+
+            client = GithubOrgClient("google")
+            result = client._public_repos_url
+            self.assertEqual(
+                result, "https://api.github.com/orgs/google/repos")
+            mock_org.assert_called_once()

1e0d55292bd9b39196a220745a22cb47c4b79937
Merch7x <aquamaingi@gmail.com>
2024-09-13 19:35:27 +0300

    Test the org method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+"""Implementing unittests through the
+   unittest module
+"""
+from client import GithubOrgClient
+import unittest
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test requests to an external server"""
+
+    @parameterized.expand([
+        ("google", {"payload": True}),
+        ("abc", {"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_org(self, org_name, payload, mock_get):
+        """test org returns a json payload using get_json"""
+        mock_response = MagicMock()
+        mock_response.json.return_value = payload
+        mock_get.return_value = mock_response
+
+        result = GithubOrgClient(org_name).org
+
+        self.assertEqual(result, payload)
+
+        mock_get.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")

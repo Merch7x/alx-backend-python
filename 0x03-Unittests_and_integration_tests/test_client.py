ae8d03bb0668564b2b8ac3976592c0aa3e285c7e
Merch7x <aquamaingi@gmail.com>
2024-09-13 21:48:07 +0300

    Test has license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -63,3 +63,11 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_public_repo_url.assert_called_once()
             mock_get.assert_called_once_with(
                 "https://api.github.com/orgs/google/repos")
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, lkey, expected):
+        """Test whether a repo has a LICENSE"""
+        self.assertEqual(GithubOrgClient.has_license(repo, lkey), expected)
